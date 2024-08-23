8d7dbed07d9e557c818868ed0f08931eb8d8b2d9
mahmodul hassan <you@example.com>
2024-08-23 18:15:44 +0600

    input form completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,9 @@
 module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
-      extend: {},
+      extend: {
+        'discussBanner': "url('/assets/bg.jpg')",
+      },
     },
     plugins: [],
   }
